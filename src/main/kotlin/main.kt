afad7c2411aebd10f5180e1faec1905c7b378f42
Tomas <mtvelasquezg@unal.edu.co>
2021-02-18 14:42:45 -0500

    added main menu

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,26 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println(texto())
+}
+
+fun texto(): Int {
+    val responseNum : Int
+    val menu ="""
+    :: Bienvenido a Recipe Maker ::
+                 
+    Selecciona la opción deseada
+    1. Hacer una receta
+    2. Ver mis recetas
+    3. Salir
+    """.trimIndent()
+
+    // print menu and read the answer
+    println(menu)
+    val response:String = readLine() ?: ""
+
+    responseNum = try {
+        response.toInt()
+    } catch (e:NumberFormatException){
+        -1
+    }
+    return responseNum
 }
